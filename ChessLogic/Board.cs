212078d3e5709b70fcce5e40f484ab1a93baeefe
FildaMilda <pajonk.filip@gmail.com>
2024-09-05 13:07:53 +0200

    Checking Legal Moves (not all of them yet)

--- ChessLogic/Board.cs
+++ ChessLogic/Board.cs
@@ -23,6 +23,18 @@
             return board;
         }
 
+        public Board Copy()
+        {
+            Board copy = new Board();
+
+            foreach (Position pos in PiecePositions())
+            {
+                copy[pos] = this[pos].Copy();
+            }
+
+            return copy;
+        }
+
         private void AddStartPieces()
         {
             this[0, 0] = new Rook(Player.Black);
@@ -59,5 +71,36 @@
         {
             return this[pos] == null;
         }
+
+        public IEnumerable<Position> PiecePositions()
+        {
+            for (int r = 0; r < 8; r++)
+            {
+                for (int c = 0; c < 8; c++)
+                {
+                    Position pos = new Position(r, c);
+
+                    if (!IsEmpty(pos))
+                    {
+                        yield return pos;
+                    }
+                }
+            }
+        }
+
+        public IEnumerable<Position> PiecePositionsFor(Player player)
+        {
+            return PiecePositions().Where(pos => this[pos].Color == player);
+        }
+
+        public bool IsInCheck(Player player)
+        {
+            return PiecePositionsFor(player.Opponent()).Any(pos =>
+            {
+                Piece piece = this[pos];
+                return piece.CanCaptureOpponentKing(pos, this);
+            });
+        }
+
     }
 }
--- ChessLogic/obj/Release/net8.0/ChessLogic.AssemblyInfo.cs
+++ ChessLogic/obj/Release/net8.0/ChessLogic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChessLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dbaa82d5b19985025031046b27395ed02f510c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ffa8bb28b8250e346ed34cb72e6572557ed4949")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChessLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChessLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
